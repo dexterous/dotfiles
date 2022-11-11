edf015700092f5f8be3fc13dd0a28b8720d22c3a
Saager Mhatre <saager.mhatre@gmail.com>
2022-11-10 21:01:38 -0500

    Update lein config
    - add plugin how-to-ns
    - add plugin antq
    - stage dep tools.namespace

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,3 +1,5 @@
-{:user {:plugins [[com.gfredericks/lein-how-to-ns "0.2.9"]
+{:user {;; :dependencies [[org.clojure/tools.namespace "1.3.0"]]
+        :plugins [[com.gfredericks/lein-how-to-ns "0.2.9"]
+                  [com.github.liquidz/antq "2.1.939"]
                   [lein-exec "0.3.7"]]
-        :dependencies [[org.clojure/tools.namespace "1.3.0"]]}}
+        :antq {}}}
